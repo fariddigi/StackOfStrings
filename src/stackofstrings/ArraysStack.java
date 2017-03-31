3e460c0d198090412e16dee69307324c429b4866
Farid R <farid.rahimov@yahoo.com>
2017-03-31 12:33:39 -0700

    Linked list and array implementation of Stacks

--- src/stackofstrings/ArraysStack.java
+++ src/stackofstrings/ArraysStack.java
@@ -26,8 +26,9 @@ public class ArraysStack {
     }
     
    public String pop(){
-       
-        return s[--N];
+       String item = s[--N];
+       s[N]= null;
+       return s[--N];
    }
    
    public boolean isEmpty(){
